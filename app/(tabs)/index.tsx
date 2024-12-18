748d2981a8f68266df3c26a8f8730f11e76618d6
Jesse Biroscak <hackajesse@users.noreply.github.com>
2024-12-18 09:30:12 -0500

    add image to home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+
+const PlaceholderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hi Mila. Boo boo.</Text>
+      <View style={styles.imageContainer}>
+      <Image source={PlaceholderImage} style={styles.Image} />
+      </View>
     </View>
   );
 }
@@ -15,12 +20,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#25292e"
   },
-  text: {
-    color: '#fff',
+  Image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
   },
 });
