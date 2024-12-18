6f2a178765bc5616d720a8d9947e0a8bcbf71281
Jesse Biroscak <hackajesse@users.noreply.github.com>
2024-12-17 20:38:23 -0500

    updated tab styles

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,25 +1,37 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle: {
+          backgroundColor: "#25292e",
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+          backgroundColor: "#25292e",
+        },
+      }}
+    >
       <Tabs.Screen 
-        name="(tabs)" 
+        name="index" 
         options={{
-          headerTitle: "ExpoTest",
-          headerLeft: () => <></>,
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
         }} 
-      />
+      /> 
       <Tabs.Screen 
         name="about" 
         options={{
-          headerTitle: "About",
-        }} 
-      />
-      <Tabs.Screen 
-        name="+not-found" 
-        options={{
-          headerTitle: "Whoopsy Daisy!",
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+          ),
         }} 
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,9 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hi Mila. Boo boo.</Text>
-      <Link href="/about" style={styles.button}>
-        About
-      </Link>
     </View>
   );
 }
