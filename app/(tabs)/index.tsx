fbe6e41e73f67f0046fe1af35ac2687cfecfbec2
Jesse Biroscak <hackajesse@users.noreply.github.com>
2024-12-18 10:06:31 -0500

    add button component and style

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Image } from "expo-image";
+import { View, StyleSheet } from 'react-native';
+import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
 
-const PlaceholderImage = require("../../assets/images/background-image.png");
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-      <Image source={PlaceholderImage} style={styles.Image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button theme='primary' label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -16,16 +22,14 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#25292e"
-  },
-  Image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+    alignItems: 'center',
+    backgroundColor: '#25292e'
   },
   imageContainer: {
     flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
