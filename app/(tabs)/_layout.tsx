bb10b07199730b5efc3849968ca0834082257a82
Jesse Biroscak <hackajesse@users.noreply.github.com>
2024-12-18 09:02:32 -0500

    adjust status bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,7 +19,7 @@ export default function TabsLayout() {
       <Tabs.Screen 
         name="index" 
         options={{
-          title: 'Home',
+          title: 'ExpoTest',
           tabBarIcon: ({ color, focused }) => (
             <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
           ),
